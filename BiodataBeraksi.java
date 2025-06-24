4f00d0273fcea2368b1cfd839f3e958d115545d5
Jefrry70 <muhammjefryy21@gmail.com>
2025-06-24 16:29:31 +0800

    membuat objek

---
+++ BiodataBeraksi.java
@@ -0,0 +1,9 @@
+public class BiodataBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek dari class Biodata
+        Biodata copilot = new Biodata("Muhammad Jefry", "2310010647", "Teknik Informatika", 2025);
+
+        // Menampilkan biodata
+        copilot.tampilkanBiodata();
+    }
+}
